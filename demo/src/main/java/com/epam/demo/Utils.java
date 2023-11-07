fa0faf4789c73b102f2d69ccc42491a5146ad890
Vitali Kalaskou <kolosokvit@gmail.com>
2023-11-07 22:39:03 +0300

    First attempt

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        for (String s : args) {
+            if (s.startsWith("-")) {
+                return false;
+            }
+        }
+        return true;
     }
 }
