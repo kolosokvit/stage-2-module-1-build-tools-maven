9bba5a488b0392126e5f0cb0040098c4f2f4ffc5
Vitali Kalaskou <kolosokvit@gmail.com>
2023-11-07 23:38:45 +0300

    Add zero check

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return !str.startsWith("-");
+        return str.equals("0") || !str.startsWith("-");
     }
 }
