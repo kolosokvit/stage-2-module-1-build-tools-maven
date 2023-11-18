407e23428642e16f65b613ef2686d05aa8d7e7b3
Vitali Klaskou <73692256+kolosokvit@users.noreply.github.com>
2023-11-18 23:04:57 +0300

    Update StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,9 @@ public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
             double number = Double.parseDouble(str);
+            if (str.startsWith("0") && number >= 1) {
+                return false;
+            }
             return number > 0;
         } catch (NumberFormatException | NullPointerException e) {
             e.printStackTrace();
